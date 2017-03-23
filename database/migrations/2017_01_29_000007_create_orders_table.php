463e3db7efdceedb63539b8413a335721d38edb8
Prakruthi <Prakruthi>
2017-03-23 14:39:42 -0500

    Revert "Merge branch 'master' into Prakruthi"
    This reverts commit e0b5a8d96601cb073a073f4ee6cb9b9be1ee0088, reversing
    changes made to 85ceea4f85a871c3751358f8dcc17af79ec3594b.

--- database/migrations/2017_01_29_000007_create_orders_table.php
+++ database/migrations/2017_01_29_000007_create_orders_table.php
@@ -20,17 +20,17 @@ class CreateOrdersTable extends Migration
             $table->integer('cntr_id');
             $table->integer('ca_id')->unsigned()->nullable();
             $table->string('order_description')->nullable();
-            $table->timestamp('order_date_created')->date();
+            $table->string('order_date_created')->default('2017-03-01');
             $table->string('order_priority')->nullable();
             $table->string('order_status')->nullable();
-            $table->decimal('order_total_cost', 8,2)->default(0.00);
- //           $table->timestamp('deleted_at')->CurrentTimestamp();
+            $table->float('order_total_cost', 8,2)->default(0.00)->nullable();
+            $table->timestamps('deleted_at');
             $table->string('resident_comment')->nullable();
-            $table->string('last_status_modified')->nullable();
-            $table->timestamp('last_status_modified_time')->nullable();
+            $table->string('last_status_modified')->default('2017-03-01');
+            $table->dateTime('last_status_modified_time')->default('2017-03-01');
             $table->integer('issue_type');
             $table->string('requestor_name')->nullable();
-            $table->softDeletes()->CurrentTimestamp();
+            $table->softDeletes();
 
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
--- resources/views/CreateCntr/index.blade.php
+++ resources/views/CreateCntr/index.blade.php
@@ -8,23 +8,17 @@
                     <div class="panel-heading">
                         <div class="pull-right">
                             <form action="{{ url('/center/create') }}" method="GET">{{ csrf_field() }}
-
-                                <button type="submit" id="create-user" class="btn btn-primary"><i
-                                            class="fa fa-btn fa-file-o"></i>Create Center
-                                </button>
-
+                                <button type="submit" id="create-user" class="btn btn-primary"><i class="fa fa-btn fa-file-o"></i>Create</button>
                             </form>
                         </div>
                         <div><h4>Center Information</h4></div>
                     </div>
-
                     <div class="panel-body">
                         {{--<div class="pull-left">--}}
                         @if (count($createcntrs) > 0)
                             <div class="table-responsive">
                                 <table class="table table-bordered table-striped cds-datatable">
                                     <thead>
-                                    <meta name="csrf-token" content="{{ csrf_token() }}">
                                     {{--<tr>--}}
                                         <th>Center Name</th>
                                         <th>Center Address1</th>
@@ -34,7 +28,7 @@
                                         <th>Center Zip</th>
                                         <th>Center Phone</th>
                                         <th>Center Fax</th>
-                                        <th style="width: 300px;">Actions</th>
+                                        <th style="width: 200px;">Actions</th>
                                     {{--</tr>--}}
                                     </thead>
                                     <tbody>
@@ -64,16 +58,14 @@
                                             <td class="table-text"><div>{{ $createcntr->cntr_phone}}</div></td>
                                             <td class="table-text"><div>{{ $createcntr->cntr_fax}}</div></td>
 
-
                                             <td class="table-text"><div><a href="{{url('center',$createcntr->id)}}" class="btn btn-primary ">View</a>
                                             <a href="{{url('center/update', $createcntr->id)}}"class="btn btn-warning">Modify</a>
-                                            <a href="{{url('center/destroy',$createcntr->id)}}" onclick='return confirm("Are you sure?") 'class="btn btn-danger">Delete</a>
+                                            <a href="{{url('center/destroy',$createcntr->id)}}"class="btn btn-danger">Delete</a>
                                             </div></td>
 
                                         </tr>   {{--{!! Form::open(['method' => 'DELETE', 'route'=>['apartment.destroy', $createapt->id],'onsubmit' => 'return ConfirmDelete()']) !!}--}}
                                                 {{--{!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}--}}
                                                 {{--{!! Form::close() !!}--}}
-
 
                                     @endforeach
 
