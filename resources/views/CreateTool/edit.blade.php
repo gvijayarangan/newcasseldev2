64a263b6b3abd6bbb768f5815607c1fcaa0dd553
Gopinath Vijayarangan <gvijayarangan@unomaha.edu>
2017-04-11 13:18:55 -0500

    Back button changes and migration fies for SP's
    Back button changes and migration fies for SP's

--- resources/views/CreateTool/edit.blade.php
+++ resources/views/CreateTool/edit.blade.php
@@ -5,7 +5,12 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading text-center" > Update Apartment Information</div>
+                    <div class="pull-left">
+                        <form action="{{ URL::previous() }}" method="GET">{{ csrf_field() }}
+                            <button type="submit" id="edit-resident" class="btn btn-primary"><i class="fa fa-btn fa-file-o"></i>Back</button>
+                        </form>
+                    </div>
+                    <div class="panel-heading text-center" > Update Tool Information</div>
                     <div class="panel-body">
     @if (count($errors) > 0)
         <div class="alert alert-danger">
