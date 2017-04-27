a0e40b056c0a477208de9d0744358697a066a4b9
chethandb <cdevarahallibalakr@unomaha.edu>
2017-04-27 10:23:41 -0500

    Fix for work oder design admin page change1 and center seed change
    Fix for work oder design admin page change1 and center seed change

--- resources/views/WorkOrder/workorder.blade.php
+++ resources/views/WorkOrder/workorder.blade.php
@@ -15,7 +15,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-10">
+            <div class="col-sm-12 col-md-10 col-md-offset-2">
                 <br> <br>
                 <div class="panel panel-default">
                     <div class="panel-heading"> Work Order Form</div>
@@ -29,120 +29,130 @@
                         </div>
                     @endif
 
-                    <div class="panel-body" style="padding-left: 15%">
+                    <div class="panel-body">
                         <input type="hidden" name="_token" value="{{ Session::token() }}">
                         <input type="hidden" name="supplyData" id="supplyData" value="">
 
                         {!! Form::label('requester', 'Requestor:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-sm-4">
-                            {!! Form::text('requestor_name',null,['class'=>'form-control input-sm'], array('id' => 'requestername')) !!}
-                        </div.panel-heading>
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
+                            {!! Form::text('requestor_name',null,['class'=>'form-control input-sm','placeholder' => 'Name'],
+                            array('id' => 'requestername')) !!}
+                        </div>
 
                         </br> </br>
-                        <span style="color: red; display:block; float:left">*</span>
 
-                        {!! Form::label('cntr_name', 'Center Name:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
 
+                        {!! Html::decode(Form::label('cntr_name', '<span style="color: red;">*</span>Center Name:', ['class' => 'col-md-3 control-label'])) !!}
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             <div class="form-group">
                                 {{ Form::select('cntr_name', array_merge([0 => 'Please Select']) + $centers, 'default',
-                                 array('id' => 'center_dropdown', 'class' => 'col-md-4','required' => 'required')) }}
+                                ['class' => 'col-md-offset-3 col-md-6 col-md-pull-3','required' => 'required'],
+                                 array('id' => 'center_dropdown')) }}
                             </div>
-                        </div.panel-heading>
+                        </div>
 
                         </br> </br>
 
 
                         {!! Form::label('apartment no', 'Apartment No:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             {{ Form::select('apt_id', array_merge([0 => 'Please Select']), 'default',
-                            array('id' => 'apartment_dropdown', 'class' => 'col-md-4')) }}
-                        </div.panel-heading>
+                            ['class' => 'col-md-offset-3 col-md-6 col-md-pull-3'],
+                            array('id' => 'apartment_dropdown')) }}
+                        </div>
 
                         </br> </br>
 
                         {!! Form::label('residentname', 'Resident Name:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             {{ Form::select('residentname', array_merge([0 => 'Please Select']),
-                            'default', array('id' => 'residentname_dropdown', 'class' => 'col-md-4')) }}
-                        </div.panel-heading>
+                            'default', ['class' =>'col-md-offset-3 col-md-6 col-md-pull-3'],
+                            array('id' => 'residentname_dropdown')) }}
+                        </div>
 
                         </br> </br>
 
                         {!! Form::label('commonarea', 'Common Area/System:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             {{ Form::select('ca_id', array_merge([0 => 'Please Select']), 'default',
-                            array('id' => 'commonarea_dropdown', 'class' => 'col-md-4')) }}
-                        </div.panel-heading>
+                            ['class' =>'col-md-offset-3 col-md-6 col-md-pull-3'],
+                            array('id' => 'commonarea_dropdown')) }}
+                        </div>
 
 
                         </br> </br>
 
                         {!! Form::label('resident_comments', 'Resident Comments:' ,['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-6">
+                        <div class="col-md-offset-1 col-md-8 col-md-pull-1">
                             {!! Form::text('resident_comments',null,
-                            array('id' => 'resident_comments','readonly' => true,'size'=>70)) !!}
-                          </div.panel-heading>
+                            ['class'=>'col-md-offset-1 col-md-8 col-md-pull-1 form-control','readonly'],
+                            array('id' => 'resident_comments')) !!}
+                        </div>
 
                         </br> </br>
 
 
                         {!! Form::label('status', 'Status:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-6">
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             {!! Form::select('order_status', ['Open' => 'Open'],
-                              'default', array('class' => 'col-md-6')) !!}
-                        </div.panel-heading>
+                              'default', array('class' => 'col-md-offset-3 col-md-6 col-md-pull-3')) !!}
+                        </div>
 
                         </br> </br>
 
 
                         {!! Form::label('priority', 'Priority:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             {!! Form::select('order_priority', ['Please Select' => 'Please Select', 'Low' => 'Low', 'Moderate' => 'Moderate', 'High' => 'High'],
-                            'default', array('class' => 'col-md-4')) !!}
-                        </div.panel-heading>
+                            'default', array('class' => 'col-md-offset-3 col-md-6 col-md-pull-3')) !!}
+                        </div>
 
 
                         </br> </br>
 
 
-
-
                         {!! Form::label('issuetype', 'Issue Type:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-4">
-                            {{ Form::select('issuetype', array_merge([0 => 'Please Select']) + $issuetypes, 'default', array('id' => 'issuetype_dropdown')) }}
-                        </div.panel-heading>
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
+                            {{ Form::select('issuetype', array_merge([0 => 'Please Select']) + $issuetypes, 'default',
+                            ['class'=>'col-md-offset-3 col-md-6 col-md-pull-3'],
+                            array('id' => 'issuetype_dropdown')) }}
+                        </div>
 
                         </br> </br>
 
                         {!! Form::label('issuedescription', 'Issue Description:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
-                            {!! Form::text('issuedescription',null, array('id' => 'issuedescription', 'readonly' => true,'size'=>70)) !!}
-                        </div.panel-heading>
+                        <div class="col-md-offset-1 col-md-8 col-md-pull-1">
+                            {!! Form::text('issuedescription',null,
+                            ['class'=>'col-md-offset-1 col-md-8 col-md-pull-1 form-control','readonly'],
+                            array('id' => 'issuedescription')) !!}
+                        </div>
 
                         </br> </br>
 
                         {!! Form::label('wodescription', 'Work Order Description:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-8">
-                            {!! Form::text('order_description',null,['class'=>'form-control']) !!}
-                        </div.panel-heading>
+                        <div class="col-md-offset-1 col-md-8 col-md-pull-1">
+                            {!! Form::text('order_description',null,
+                            ['class'=>'form-control col-md-offset-1 col-md-8 col-md-pull-1','placeholder' => 'Briefly describe your issue']) !!}
+                        </div>
 
                         </br> </br>
 
                         {!! Form::label('assigntype', 'Assign To:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading class="col-md-6">
+                        <div class="col-md-offset-3 col-md-6 col-md-pull-3">
                             {{ Form::select('assign_user_id', array_merge([0 => 'Please Select']) + $workers, 'default',
-                             array('id' => 'assigntype_dropdown','class' => 'col-md-6')) }}
-                        </div.panel-heading>
+                             ['class'=>'col-md-offset-3 col-md-6 col-md-pull-3'],
+                             array('id' => 'assigntype_dropdown')) }}
+                        </div>
 
                         </br> </br>
 
 
                         {!! Form::label('toolsused', 'Tools used:', ['class' => 'col-md-3 control-label']) !!}
-                        <div.panel-heading style="padding-left: 15px">
+                        <div class="col-md-offset-1 col-md-8 col-md-pull-1">
                             {{ Form::select('toolsused_id[]', $toolsdata,
-                              'default', array('id' => 'tools_data[]', 'multiple'=>'multiple', 'style' =>'width:75%')) }}
-                        </div.panel-heading>
+                              'default', ['class'=>'col-md-offset-1 col-md-8 col-md-pull-1','placeholder' => 'Please select'],
+                              array('id' => 'tools_data[]', 'multiple'=>'multiple')) }}
+                        </div>
 
                         </br> </br>
                     </div>
@@ -160,19 +170,20 @@
                                            'default', array('id' => 'supply_dropdown')) }}
                                         </div>
                                     </div>
-                                 <div class="form-group">
+                                    <div class="form-group">
 
                                         <label for="amount" class="col-sm-3 control-label">Unit Price</label>
 
-                                         <div class= "col-sm-6 input-group" >
-                                             <span class="input-group-addon">$</span>
+                                        <div class="col-sm-6 input-group">
+                                            <span class="input-group-addon">$</span>
 
-                                       <input type="text" class="form-control" placeholder="unitprice" id="unitprice" name="unitprice"
-                                                readonly>
+                                            <input type="text" class="form-control" placeholder="unitprice"
+                                                   id="unitprice" name="unitprice"
+                                                   readonly>
 
-                                       </div>
+                                        </div>
 
-                                </div>
+                                    </div>
 
                                     <div class="form-group">
                                         <label for="description" class="col-sm-3 control-label">Unit</label>
@@ -283,13 +294,13 @@
                 $("#apartment_dropdown").empty();
 
                 apartment_data.append($("<option></option>")
-                        .attr("value", 0)
-                        .text("Please Select"));
+                    .attr("value", 0)
+                    .text("Please Select"));
 
                 $.each(data, function (key, value) {
                     apartment_data.append($("<option></option>")
-                            .attr("value", key)
-                            .text(value));
+                        .attr("value", key)
+                        .text(value));
                 });
                 $('#apartment_dropdown').val(0).change();
 
@@ -302,13 +313,13 @@
                 $("#commonarea_dropdown").empty();
 
                 commonarea_data.append($("<option></option>")
-                        .attr("value", 0)
-                        .text("Please Select"));
+                    .attr("value", 0)
+                    .text("Please Select"));
 
                 $.each(data, function (key, value) {
                     commonarea_data.append($("<option></option>")
-                            .attr("value", key)
-                            .text(value));
+                        .attr("value", key)
+                        .text(value));
                 });
                 $('#commonarea_dropdown').val(0).change();
 
@@ -330,15 +341,15 @@
                     if (data.length != 0) {
                         $.each(data, function (key, value) {
                             resident_data.append($("<option></option>")
-                                    .attr("value", key)
-                                    .text(value));
+                                .attr("value", key)
+                                .text(value));
                         });
                         //Show the first index upon change
                         $('#residentname_dropdown').val(Object.entries(data)[0][0]).change();
                     } else {
                         resident_data.append($("<option></option>")
-                                .attr("value", 0)
-                                .text("Resident not occupied"));
+                            .attr("value", 0)
+                            .text("Resident not occupied"));
                         $('#residentname_dropdown').val(0).change();
                     }
                 });
@@ -346,8 +357,8 @@
                 //Empty resident information
                 $("#residentname_dropdown").empty();
                 $("#residentname_dropdown").append($("<option></option>")
-                        .attr("value", 0)
-                        .text("Please Select"));
+                    .attr("value", 0)
+                    .text("Please Select"));
                 $('#residentname_dropdown').val(0).change();
 
                 //Disable commonarea dropdown
@@ -407,12 +418,12 @@
 
 
         $('#addDetails').click(function () {
-            if ($("#supply_dropdown option:selected").val() != 0 && $("#unit").val() != '' && $("#unit").val() >0 ) {
+            if ($("#supply_dropdown option:selected").val() != 0 && $("#unit").val() != '' && $("#unit").val() > 0) {
 
                 var order_data = {};
                 order_data["SupplyName"] = $("#supply_dropdown option:selected").text();
                 order_data["unit"] = $("#unit").val();
-                order_data["unitPrice"] = "$"+$("#unitprice").val();
+                order_data["unitPrice"] = "$" + $("#unitprice").val();
                 order_data["total"] = $("#total").val();
                 order_data["remove-row"] = '<span class="glyphicon glyphicon-remove"></span>';
 
@@ -432,25 +443,25 @@
                 $("#supply_dropdown option:eq(0)").prop("selected", true).change();
 
 
-                var tableData = $.param($('#dataSupplyTable td').map(function() {
+                var tableData = $.param($('#dataSupplyTable td').map(function () {
                     return {
                         name: $(this).attr('name'),
                         value: $(this).text().trim()
                     };
                 }));
 
-                $("#supplyData").val(""+tableData+"");
+                $("#supplyData").val("" + tableData + "");
                 console.log(tableData);
             }
         });
 
         $('#unit').change(function () {
 
-            if($('#unit').val()>0){
+            if ($('#unit').val() > 0) {
 
-            var totalAmount = $('#unit').val() * $('#unitprice').val();
-            totalAmount = totalAmount.toFixed(2);
-            $('#total').val(totalAmount);
+                var totalAmount = $('#unit').val() * $('#unitprice').val();
+                totalAmount = totalAmount.toFixed(2);
+                $('#total').val(totalAmount);
             }
         });
 
@@ -458,14 +469,14 @@
             var tr = $(this).closest('tr');
             tr.remove();
             calculateTotalAmount();
-            var tableData = $.param($('#dataSupplyTable td').map(function() {
+            var tableData = $.param($('#dataSupplyTable td').map(function () {
                 return {
                     name: $(this).attr('name'),
                     value: $(this).text().trim()
                 };
             }));
 
-            $("#supplyData").val(""+tableData+"");
+            $("#supplyData").val("" + tableData + "");
             console.log(tableData);
         });
 
